69a0db78a4910e19287f65d40114fad0c00fde3e
roylines <roy@roylines.co.uk>
2018-10-06 11:59:33 +0100

    rename of output

--- terraform.tf
+++ terraform.tf
@@ -65,7 +65,7 @@ resource "digitalocean_droplet" "mosquitto" {
 	user_data = "${data.template_file.user_data.rendered}"
 }
 
-output "mosquitto" {
+output "ipv4_address" {
   description = "the ipaddress of the mosquitto server"
   value = "${digitalocean_droplet.mosquitto.ipv4_address}"
 }
